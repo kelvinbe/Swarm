b0be7bde5f45f7743cab4bf5b6eaa046b81a9723
kelvinbe <kelvin.beno.k@gmail.com>
2024-02-26 19:32:39 +0300

    updates on properties page

--- app/(tabs)/(properties)/home.tsx
+++ app/(tabs)/(properties)/home.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import { Text, ScrollView, View, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, ScrollView, FlatList, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import SearchAndFilter from '../../../components/Molecules/Topics/SearchAndFilter';
 import { useTheme } from "@rneui/themed";
 import { useState } from "react";
 import { Button } from "@rneui/base";
 import Animated, { withTiming } from 'react-native-reanimated';
 import { useSharedValue, withSpring, useAnimatedStyle, useAnimatedProps } from 'react-native-reanimated';
 import { transform } from 'lodash';
-import { Circle, Svg } from 'react-native-svg';
+import { images } from '../../../data';
+
+// Sample data for the cards
+const miniCardsData = [
+  { id: '1', icon: 'random', text: 'Rent' },
+  { id: '2', icon: 'money', text: 'Buy' },
+  { id: '3', icon: 'sellsy', text: 'Sell' },
+];
+
 
 
 
@@ -18,7 +28,6 @@ const Swarms = () => {
 
   const r = useSharedValue(20)
 
-  const AnimatedCircle = Animated.createAnimatedComponent(Circle)
 
   const handlePress = () => {
     r.value += 10;
@@ -32,12 +41,66 @@ const Swarms = () => {
 
   
   return (
-    <>
-  <Svg style={styles.svg}>
-    <AnimatedCircle cx='50%' cy='50%' r={r} fill='blue' animatedProps={animatedProps}/>
-  </Svg>
-  <Button onPress={handlePress} title="Click me" />
-  </>
+    <ScrollView style={styles.container}>
+      <View>
+        <SearchAndFilter searchVisible filterVisible />
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+        {miniCardsData.map(card => (
+          <View style={styles.miniCardsData}  key={card.id}>
+            <FontAwesome name={card.icon} size={24} />
+            <Text style={{padding: 5}}>{card.text}</Text>
+          </View>
+        ))}
+      </View>
+
+      <Text style={styles.headerText} >Popular Near You</Text>
+      <FlatList
+        horizontal
+        data={images}
+        keyExtractor={item => item.title}
+        renderItem={({ item }) => (
+          <View  style={styles.imageCard}>
+            <Image source={{uri: item.imgUrl}} style={{ width: 140, height: 150, borderRadius: 10 }} />
+            <TouchableOpacity style={{ position: 'absolute', top: 5, right: 5 }}>
+              <FontAwesome name="heart" size={16} color="red" />
+            </TouchableOpacity>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
+
+      {/* Curated For You */}
+      <Text style={styles.headerText}>Curated For You</Text>
+      <FlatList
+        horizontal
+        data={images}
+        keyExtractor={item => item.title}
+        renderItem={({ item }) => (
+          <View style={styles.imageCard}>
+            <Image source={{uri: item.imgUrl}} style={{ width: 140, height: 150, borderRadius: 10 }} />
+            <TouchableOpacity style={{ position: 'absolute', top: 5, right: 5 }}>
+              <FontAwesome name="heart" size={16} color="red" />
+            </TouchableOpacity>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
+
+      {/* Recommended For You */}
+      <Text style={styles.headerText}>Recommended For You</Text>
+      <FlatList
+        data={images}
+        keyExtractor={item => item.title}
+        renderItem={({ item }) => (
+          <View style={styles.recommendedFor}>
+            <Image source={{uri: item.imgUrl}} style={{ width: 140, height: 125, borderRadius: 10 }} />
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
+    </ScrollView>
   )
   
 };
@@ -45,16 +108,60 @@ const Swarms = () => {
 export default Swarms;
 
 const styles = StyleSheet.create({
+  
+  container: {
+    flex: 1,
+    padding: 4,
+    backgroundColor: 'white'
+
+  },
 
   box: {
     width: 120,
-    height: 120,
+    height: 125,
     backgroundColor: 'violet',
     borderRadius: 20,
     marginVertical: 50
-  }, svg: {
+  }, 
+  svg: {
     height: 250,
     width: '100%',
   },
+  miniCardsData: {
+    width: 120,
+    height: 40,
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 10
+  },
+  imageCard: {
+    margin: 10,
+    flexDirection: 'column',
+    width: 140,
+    height: 170,
+    borderWidth: 2,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10
+  },
+  recommendedFor: {
+      margin: 10,
+      flexDirection: 'row',
+      width: 370,
+      height: 130,
+      borderWidth: 2,
+      borderRadius: 10,
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+    }
+  
 
 })
